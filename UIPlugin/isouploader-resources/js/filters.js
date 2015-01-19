674859214336834ddb5522a48592e5190e3915f8
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-01-19 09:39:53 +0800

    Fix bug when the file is empty.

--- UIPlugin/isouploader-resources/js/filters.js
+++ UIPlugin/isouploader-resources/js/filters.js
@@ -10,6 +10,7 @@
       if (typeof precision === 'undefined') precision = 1;
       var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
       number = Math.floor(Math.log(bytes) / Math.log(1024));
+      if (bytes === 0) { return '0 bytes' }
       return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
     }
   });
