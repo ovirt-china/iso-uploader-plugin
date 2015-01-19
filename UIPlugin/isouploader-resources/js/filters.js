002d69dc0689f59add1f15935f7886f12e2d29cf
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-01-19 09:43:21 +0800

    Make the code more readable.

--- UIPlugin/isouploader-resources/js/filters.js
+++ UIPlugin/isouploader-resources/js/filters.js
@@ -3,14 +3,24 @@
 (function() {
 
   var app = angular.module('plugin.filters', []);
-
+  
   app.filter('bytes', function() {
     return function(bytes, precision) {
-      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-      if (typeof precision === 'undefined') precision = 1;
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
+        return '-';
+      }
+
+      if (typeof precision === 'undefined') {
+        precision = 1;
+      }
+
+      if (bytes === 0) {
+        return '0 bytes';
+      }
+
       var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
       number = Math.floor(Math.log(bytes) / Math.log(1024));
-      if (bytes === 0) { return '0 bytes' }
+
       return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
     }
   });
