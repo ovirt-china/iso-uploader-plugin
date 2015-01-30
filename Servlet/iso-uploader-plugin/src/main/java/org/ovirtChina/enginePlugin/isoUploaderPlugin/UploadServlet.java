b8f2b586669b0bc563c038e613deda9557b70973
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-01-30 18:12:25 +0800

    Change resumable* to flow* in the file.

--- Servlet/iso-uploader-plugin/src/main/java/org/ovirtChina/enginePlugin/isoUploaderPlugin/UploadServlet.java
+++ Servlet/iso-uploader-plugin/src/main/java/org/ovirtChina/enginePlugin/isoUploaderPlugin/UploadServlet.java
@@ -10,12 +10,12 @@ import java.io.InputStream;
 import java.io.RandomAccessFile;
 
 import org.ovirtChina.enginePlugin.isoUploaderPlugin.HttpUtils;
-import org.ovirtChina.enginePlugin.isoUploaderPlugin.ResumableInfo;
-import org.ovirtChina.enginePlugin.isoUploaderPlugin.ResumableInfoStorage;
+import org.ovirtChina.enginePlugin.isoUploaderPlugin.FlowInfo;
+import org.ovirtChina.enginePlugin.isoUploaderPlugin.FlowInfoStorage;
 
 /**
  *
- * This is a servlet demo,  for using Resumable.js to upload files.
+ * This is a servlet demo,  for using Flow.js to upload files.
  *
  * by fanxu123
  */
@@ -24,14 +24,14 @@ public class UploadServlet extends HttpServlet {
     public static final String UPLOAD_DIR = "e:\\";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int resumableChunkNumber        = getResumableChunkNumber(request);
+        int flowChunkNumber = getFlowChunkNumber(request);
 
-        ResumableInfo info = getResumableInfo(request);
+        FlowInfo info = getFlowInfo(request);
 
-        RandomAccessFile raf = new RandomAccessFile(info.resumableFilePath, "rw");
+        RandomAccessFile raf = new RandomAccessFile(info.flowFilePath, "rw");
 
         //Seek to position
-        raf.seek((resumableChunkNumber - 1) * info.resumableChunkSize);
+        raf.seek((flowChunkNumber - 1) * info.flowChunkSize);
 
         //Save to file
         InputStream is = request.getInputStream();
@@ -50,9 +50,9 @@ public class UploadServlet extends HttpServlet {
 
 
         //Mark as uploaded.
-        info.uploadedChunks.add(new ResumableInfo.ResumableChunkNumber(resumableChunkNumber));
+        info.uploadedChunks.add(new FlowInfo.FlowChunkNumber(flowChunkNumber));
         if (info.checkIfUploadFinished()) { //Check if all chunks uploaded, and change filename
-            ResumableInfoStorage.getInstance().remove(info);
+            FlowInfoStorage.getInstance().remove(info);
             response.getWriter().print("All finished.");
         } else {
             response.getWriter().print("Upload");
@@ -60,36 +60,49 @@ public class UploadServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int resumableChunkNumber        = getResumableChunkNumber(request);
+        int flowChunkNumber        = getFlowChunkNumber(request);
 
-        ResumableInfo info = getResumableInfo(request);
+        FlowInfo info = getFlowInfo(request);
 
-        if (info.uploadedChunks.contains(new ResumableInfo.ResumableChunkNumber(resumableChunkNumber))) {
+        if (info.uploadedChunks.contains(new FlowInfo.FlowChunkNumber(flowChunkNumber))) {
             response.getWriter().print("Uploaded."); //This Chunk has been Uploaded.
         } else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
     }
 
-    private int getResumableChunkNumber(HttpServletRequest request) {
-        return HttpUtils.toInt(request.getParameter("resumableChunkNumber"), -1);
+    private int getFlowChunkNumber(HttpServletRequest request) {
+        return HttpUtils.toInt(request.getParameter("flowChunkNumber"), -1);
     }
 
-    private ResumableInfo getResumableInfo(HttpServletRequest request) throws ServletException {
+    private FlowInfo getFlowInfo(HttpServletRequest request) throws ServletException {
         String base_dir = UPLOAD_DIR;
 
-        int resumableChunkSize          = HttpUtils.toInt(request.getParameter("resumableChunkSize"), -1);
-        long resumableTotalSize         = HttpUtils.toLong(request.getParameter("resumableTotalSize"), -1);
-        String resumableIdentifier      = request.getParameter("resumableIdentifier");
-        String resumableFilename        = request.getParameter("resumableFilename");
-        String resumableRelativePath    = request.getParameter("resumableRelativePath");
+        int flowChunkSize          = HttpUtils.toInt(request.getParameter("flowChunkSize"), -1);
+        long flowTotalSize         = HttpUtils.toLong(request.getParameter("flowTotalSize"), -1);
+        String flowIdentifier      = request.getParameter("flowIdentifier");
+        String flowFilename        = request.getParameter("flowFilename");
+        String flowRelativePath    = request.getParameter("flowRelativePath");
+
+        //DEBUG
+        System.out.println(
+        "base_dir = " + base_dir + "\n"
+        + "flowChunkSize = " + flowChunkSize + "\n"
+        + "flowTotalSize = " + flowTotalSize + "\n"
+        + "flowIdentifier = " + flowIdentifier + "\n"
+        + "flowFilename = " + flowFilename + "\n"
+        + "flowRelativePath = " + flowRelativePath
+        );
+        //!DEBUG
+
+
         //Here we add a ".temp" to every upload file to indicate NON-FINISHED
-        String resumableFilePath        = new File(base_dir, resumableFilename).getAbsolutePath() + ".temp";
+        String flowFilePath        = new File(base_dir, flowFilename).getAbsolutePath() + ".temp";
 
-        ResumableInfoStorage storage = ResumableInfoStorage.getInstance();
+        FlowInfoStorage storage = FlowInfoStorage.getInstance();
 
-        ResumableInfo info = storage.get(resumableChunkSize, resumableTotalSize,
-                resumableIdentifier, resumableFilename, resumableRelativePath, resumableFilePath);
+        FlowInfo info = storage.get(flowChunkSize, flowTotalSize,
+                flowIdentifier, flowFilename, flowRelativePath, flowFilePath);
         if (!info.vaild())         {
             storage.remove(info);
             throw new ServletException("Invalid request params.");
