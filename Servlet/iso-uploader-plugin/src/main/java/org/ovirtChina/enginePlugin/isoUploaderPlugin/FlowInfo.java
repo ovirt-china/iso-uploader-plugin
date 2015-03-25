8b951cec3c49bbfb360538e33126da33f12dcdaa
Lucas Vandroux <lucas.vandroux@gmail.com>
2015-03-25 16:08:07 +0800

    First Servlet Attempt

---
+++ Servlet/iso-uploader-plugin/src/main/java/org/ovirtChina/enginePlugin/isoUploaderPlugin/FlowInfo.java
@@ -0,0 +1,69 @@
+package org.ovirtChina.enginePlugin.isoUploaderPlugin;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * by fanxu
+ */
+public class FlowInfo {
+
+    public int      flowChunkSize;
+    public long     flowTotalSize;
+    public String   flowIdentifier;
+    public String   flowFilename;
+    public String   flowRelativePath;
+
+    public static class FlowChunkNumber {
+        public FlowChunkNumber(int number) {
+            this.number = number;
+        }
+
+        public int number;
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof FlowChunkNumber
+                    ? ((FlowChunkNumber)obj).number == this.number : false;
+        }
+
+        @Override
+        public int hashCode() {
+            return number;
+        }
+    }
+
+    //Chunks uploaded
+    public HashSet<FlowChunkNumber> uploadedChunks = new HashSet<FlowChunkNumber>();
+
+    public String flowFilePath;
+
+    public boolean vaild(){
+        if (flowChunkSize < 0 || flowTotalSize < 0
+                || HttpUtils.isEmpty(flowIdentifier)
+                || HttpUtils.isEmpty(flowFilename)
+                || HttpUtils.isEmpty(flowRelativePath)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public boolean checkIfUploadFinished() {
+        //check if upload finished
+        int count = (int) Math.ceil(((double) flowTotalSize) / ((double) flowChunkSize));
+        for(int i = 1; i < count + 1; i ++) {
+            if (!uploadedChunks.contains(new FlowChunkNumber(i))) {
+                return false;
+            }
+        }
+
+        //Upload finished, change filename.
+        File file = new File(flowFilePath);
+        String new_path = file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - ".temp".length());
+        file.renameTo(new File(new_path));
+        return true;
+    }
+}
---
+++ Servlet/iso-uploader-plugin/src/main/java/org/ovirtChina/enginePlugin/isoUploaderPlugin/UploadServlet.java
@@ -0,0 +1,112 @@
+package org.ovirtChina.enginePlugin.isoUploaderPlugin;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+import org.ovirtChina.enginePlugin.isoUploaderPlugin.HttpUtils;
+import org.ovirtChina.enginePlugin.isoUploaderPlugin.FlowInfo;
+import org.ovirtChina.enginePlugin.isoUploaderPlugin.FlowInfoStorage;
+
+/**
+ *
+ * This is a servlet demo,  for using Flow.js to upload files.
+ *
+ * by fanxu123
+ */
+public class UploadServlet extends HttpServlet {
+
+    public static final String UPLOAD_DIR = "e:\\";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int flowChunkNumber = getFlowChunkNumber(request);
+
+        FlowInfo info = getFlowInfo(request);
+
+        RandomAccessFile raf = new RandomAccessFile(info.flowFilePath, "rw");
+
+        //Seek to position
+        raf.seek((flowChunkNumber - 1) * info.flowChunkSize);
+
+        //Save to file
+        InputStream is = request.getInputStream();
+        long readed = 0;
+        long content_length = request.getContentLength();
+        byte[] bytes = new byte[1024 * 100];
+        while(readed < content_length) {
+            int r = is.read(bytes);
+            if (r < 0)  {
+                break;
+            }
+            raf.write(bytes, 0, r);
+            readed += r;
+        }
+        raf.close();
+
+
+        //Mark as uploaded.
+        info.uploadedChunks.add(new FlowInfo.FlowChunkNumber(flowChunkNumber));
+        if (info.checkIfUploadFinished()) { //Check if all chunks uploaded, and change filename
+            FlowInfoStorage.getInstance().remove(info);
+            response.getWriter().print("All finished.");
+        } else {
+            response.getWriter().print("Upload");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int flowChunkNumber        = getFlowChunkNumber(request);
+
+        FlowInfo info = getFlowInfo(request);
+
+        if (info.uploadedChunks.contains(new FlowInfo.FlowChunkNumber(flowChunkNumber))) {
+            response.getWriter().print("Uploaded."); //This Chunk has been Uploaded.
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private int getFlowChunkNumber(HttpServletRequest request) {
+        return HttpUtils.toInt(request.getParameter("flowChunkNumber"), -1);
+    }
+
+    private FlowInfo getFlowInfo(HttpServletRequest request) throws ServletException {
+        String base_dir = UPLOAD_DIR;
+
+        int flowChunkSize          = HttpUtils.toInt(request.getParameter("flowChunkSize"), -1);
+        long flowTotalSize         = HttpUtils.toLong(request.getParameter("flowTotalSize"), -1);
+        String flowIdentifier      = request.getParameter("flowIdentifier");
+        String flowFilename        = request.getParameter("flowFilename");
+        String flowRelativePath    = request.getParameter("flowRelativePath");
+
+        //DEBUG
+        System.out.println(
+        "base_dir = " + base_dir + "\n"
+        + "flowChunkSize = " + flowChunkSize + "\n"
+        + "flowTotalSize = " + flowTotalSize + "\n"
+        + "flowIdentifier = " + flowIdentifier + "\n"
+        + "flowFilename = " + flowFilename + "\n"
+        + "flowRelativePath = " + flowRelativePath
+        );
+        //!DEBUG
+
+
+        //Here we add a ".temp" to every upload file to indicate NON-FINISHED
+        String flowFilePath        = new File(base_dir, flowFilename).getAbsolutePath() + ".temp";
+
+        FlowInfoStorage storage = FlowInfoStorage.getInstance();
+
+        FlowInfo info = storage.get(flowChunkSize, flowTotalSize,
+                flowIdentifier, flowFilename, flowRelativePath, flowFilePath);
+        if (!info.vaild())         {
+            storage.remove(info);
+            throw new ServletException("Invalid request params.");
+        }
+        return info;
+    }
+}
